0eb738bee76b077fe46030a319d9f2bf7db3fc6b
lyric <yiyuanwang33@gmail.com>
2020-08-22 22:19:51 +0800

    add 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>当前路径不存在</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
