2ede83fa60f540333605bfeea31f5b03890b9af4
lyric <yiyuanwang33@gmail.com>
2020-08-23 12:33:16 +0800

    add Nav style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="nav">
-    <router-link to="/money">Money</router-link>|
-    <router-link to="/labels">Labels</router-link>|
-    <router-link to="/statistics">Statistics</router-link>
+    <router-link to="/money" class="a">
+      <svg class="icon">
+        <use xlink:href="#icon-money" />
+      </svg>
+      <div>记一笔aaaaaa</div>
+    </router-link>
+    <router-link to="/labels" class="a">
+      <svg class="icon">
+        <use xlink:href="#icon-label" />
+      </svg>
+      <div>标签</div>
+    </router-link>
+    <router-link to="/statistics" class="a">
+      <svg class="icon">
+        <use xlink:href="#icon-barchart" />
+      </svg>
+      <div>统计</div>
+    </router-link>
   </div>
 </template>
 
@@ -15,5 +30,23 @@ export default {};
   border: 1px solid red;
   display: flex;
   justify-content: space-evenly;
+}
+.icon {
+  width: 2em;
+  height: 2em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+a {
+  border: 1px solid black;
+  text-decoration: none;
+  color: #222;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
